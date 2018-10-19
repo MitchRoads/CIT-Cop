dac9d79e85e37ee1ae48045ae844013bcf790816
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-19 12:24:02 -0400

    Update cop.js

--- cop.js
+++ cop.js
@@ -150,7 +150,7 @@ if(!res) return message.channel.send(`I've failed to find any type of GIF that r
 let pages = ['This is page one!', 'Second page', 'Third', 'You can add pages', 'All you need to do is add another item in the array', '**Supports markdown and regular chat description property**'];
 let page = 1;
 
-
+const { MessageEmbed } = require('discord.js');
 const pageembed = new Discord.MessageEmbed()
 .setDescription(pages[page-1])
 .setColor(0x374f6b)
